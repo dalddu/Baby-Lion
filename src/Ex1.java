84bc5e039e42e5c3493d6e3dddef36ef92bab3ed
DESKTOP-J9CMK4R\USER <0313tndus@naver.com>
2022-10-04 16:35:11 +0900

    Ex1

---
+++ src/Ex1.java
@@ -0,0 +1,10 @@
+public class Ex1 {
+    public static void main(String[] args) {
+//        int refund = 25000;
+
+        for(int i=1; i<=5; i+=1) {
+            System.out.print(i + " ");
+        }
+
+    }
+}
